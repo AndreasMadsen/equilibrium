f4d8fc69cf15cfd9eca412bda2660bd5224a64ae
Joshua Holbrook <josh.holbrook@gmail.com>
2012-07-06 21:21:55 -0800

    [fix] fs.existsSync was moved to fs.existsSync

--- test/common.js
+++ test/common.js
@@ -9,7 +9,7 @@ var fs = require('fs');
 
 // node < 0.8 compatibility
 exports.exists = fs.exists || fs.exists;
-exports.existsSync = fs.existsSync || path.existsSync;
+exports.existsSync = fs.existsSync || fs.existsSync;
 
 // resolve main dirpaths
 exports.test = path.dirname(module.filename);
